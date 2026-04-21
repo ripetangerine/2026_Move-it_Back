3fb0a5da5f91d643aacf506101d5a5f33d406858
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-04-22 08:39:51 +0900

    feat: authService jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,6 +12,8 @@ import { AuthService } from './auth.service';
 import { LoginDto } from "./dto/login.dto";
 import { SignupDto } from './dto/signup.dto';
 
+// TODO : cook;
+
 @Controller('auth')
 export class AuthController{
   constructor(private readonly authService: AuthService) {}
@@ -21,11 +23,11 @@ export class AuthController{
     const user = await this.authService.validateUser( // todo; pipe
       loginDto.username,
       loginDto.password,
-    )
+    );
+
     if (!user) {
       throw new UnauthorizedException('Invalid credentials');
     }
-
     return this.authService.login(user);
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,30 +2,50 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from '@/user/user.service';
 import * as bcrypt from "bcrypt";
 import { JwtService } from 'node_modules/@nestjs/jwt';
+import { ConfigService } from 'node_modules/@nestjs/config';
 
 @Injectable()
 export class AuthService {
     constructor(
         private userService: UserService,
         private jwtService: JwtService,
+        private configService: ConfigService
     ){};
 
     async validateUser(username:string, password:string){
         const user = await this.userService.findOne(username);
-        console.log(user);
-
         if (user && await bcrypt.compare(password, user.password)) {
             const { password, ...result } = user;
             return result;
         }
+        console.log(user);
+
     }
 
     async login(user: any) {
         const payload = { username: user.username, sub: user.id };
+        
+        const token = this.jwtService.sign(payload, {
+            secret: this.configService.get('JWT_ACCESS_SECRET'),
+            expiresIn: '15m'
+        });
+        const refreshToken = this.jwtService.sign(payload, {
+            secret: this.configService.get('JWT_REFRESH_SECRET'),
+            expiresIn: '7d'
+        });
+        // await this.updateRefreshTokenInDB(user.id, refreshToken); //DB 저장 필수 ;;
 
-        return {
-            access_token: this.jwtService.sign(payload),
-        }
+        return { token, refreshToken, tokenExpires: new Date(Date.now() + 15 * 60 * 1000) };
+        // return { access_token: this.jwtService.sign(payload)}
+    }
+
+    async refresh(user: any){
+        const payload = { username: user.username, sub: user.id };
+        const newToken = this.jwtService.sign(payload, {
+            secret: this.configService.get('JWT_ACCESS_SECRET'),
+            expiresIn: '15m'
+        });
+        return { token: newToken };
     }
 
     async signIn(username:string, pass:string): Promise<any>{ //TODO: 리턴 형변환
