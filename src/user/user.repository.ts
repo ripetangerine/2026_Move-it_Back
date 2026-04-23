00a1cf588ee6b670a728e15ba421be35455f6b1e
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-04-23 09:32:55 +0900

    refactor: antipattern 제거

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { Repository } from "node_modules/typeorm";
 import { User } from "./entities/user.entitiy";
-
+import { InjectRepository } from 'node_modules/@nestjs/typeorm';
+import { Repository } from 'node_modules/typeorm';
 
 @Injectable()
-export class UserRepository extends Repository<User>{
-    private userRepository: Repository<User>;
-    // constructor(private readonly dataSource: DataSource){
-    //     this.userRepository = this.dataSource.getRepository(User);
-    // }
+export class UserRepository{
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>
+    ){}
 
-    async findById(id: User['id']): Promise<User|any> {
-        const entity = await this.findOne({
+    async findById(id: User['id']): Promise<User|null> {
+        const entity = await this.userRepository.findOne({
             where: { id: Number(id) },
         });
         return entity;
     }
 
-    async findByName(username: string): Promise<User|any>{
+    async findByName(username: string): Promise<User|null>{
         if(!username) return null;
-        return await this.findOne({
+        return await this.userRepository.findOne({
             where: { username: username }
         });
     }
