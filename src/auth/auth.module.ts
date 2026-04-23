cdc7b6fa97e543208c634a7323fd5d7bbef4a4f8
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-04-23 09:42:47 +0900

    config: auth module jwt import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserService } from '@/user/user.service';
+import { UserModule } from '@/user/user.module';
+import { PassportModule } from 'node_modules/@nestjs/passport';
+import { JwtModule } from 'node_modules/@nestjs/jwt';
 
 @Module({
+  imports:[
+    UserModule, PassportModule,
+    JwtModule.register({
+      // secret: 
+      signOptions: {expiresIn: "15m"}
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserService],
+  exports: [AuthService]
 })
 export class AuthModule {}
